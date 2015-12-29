51a8b4f8dde6e221b9047b76b5f29d95c9e90b94
eaw20024 <eric.washington.me@gmail.com>
2015-12-29 02:38:41 -0500

    Changed Production.rb File

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,4 @@ Rails.application.configure do
   Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
   Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 
-  :storage => :s3,
-  :s3_credentials => {
-  :bucket => ENV['AWS_BUCKET'], 
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-}
 end
