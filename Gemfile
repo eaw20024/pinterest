2b6506f80b6b4b73f02287993824e2ed3c39e476
eaw20024 <eric.washington.me@gmail.com>
2015-12-29 01:56:30 -0500

    Add image upload with paperclip

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '~> 3.5', '>= 3.5.2'
+gem 'paperclip', '~> 4.2'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -30,7 +30,7 @@ class PinsController < ApplicationController
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      render :edit
+      render action: 'edit'
     end
   end
 
